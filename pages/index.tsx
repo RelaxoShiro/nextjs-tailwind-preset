28a01b41ebc8933517df63a28cb9edbde4c6f25e
RelaxoShiro <tolic12380@gmail.com>
2023-02-22 02:29:34 +0100

    Initial

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+import BoxTemp from './boxTemp'
+
+export default function Home() {
+  return (
+    <>
+    <h1 className='border border-black justify-center text-center'>Template Project</h1>
+ 
+      
+    </>
+  )
+}
